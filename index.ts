2e687653049c1381fa2ea56ecd4b5925af7351cf
Bryn Ghiffar <bryn.ghiffar@gmail.com>
2024-04-22 09:19:10 +0700

    set cors anywhere

--- index.ts
+++ index.ts
@@ -16,7 +16,12 @@ const app: Express = express();
 const port = process.env["PORT"] ?? 5000;
 
 app.use(json());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
 
 type RoomId = string;
 type PeerId = string;
